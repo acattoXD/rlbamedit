d1ce87b9693d3baa74417f852750072bfdd7b6e6
a cat <98917510+acattoXD@users.noreply.github.com>
2024-03-16 00:30:08 +0100

    Update bam.ps1

--- bam.ps1
+++ bam.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "SilentlyContinue"
 function Get-Signature {
 
     [CmdletBinding()]
-     param (
+    param (
         [string[]]$FilePath
     )
 
@@ -16,7 +16,7 @@ function Get-Signature {
             $Signature = "Valid Signature"
         }
         elseif ($Authenticode -eq "NotSigned") {
-            $Signature = "Invalid Signature (NotSigned, likely a cheat)"
+            $Signature = "Invalid Signature (NotSigned)"
         }
         elseif ($Authenticode -eq "HashMismatch") {
             $Signature = "Invalid Signature (HashMismatch)"
@@ -36,90 +36,114 @@ function Get-Signature {
 
 Clear-Host
 
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
 
-function Test-Admin {;$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent());$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);}
 if (!(Test-Admin)) {
     Write-Warning "BAM Script won't work unless you run it as admin silly.."
     Start-Sleep 10
     Exit
 }
 
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-
 Clear-Host
-Write-Host -ForegroundColor Cyan "BAM Script written by acatto, stripped off of RedLotus.";
-Write-Host "";
-
+Write-Host -BackgroundColor Red -ForegroundColor Cyan "BAM Script written by account, stripped off of RedLotus."
+Write-Host ""
 
 $sw = [Diagnostics.Stopwatch]::StartNew()
 
-if (!(Get-PSDrive -Name HKLM -PSProvider Registry)){
-    Try{New-PSDrive -Name HKLM -PSProvider Registry -Root HKEY_LOCAL_MACHINE}
-    Catch{Write-Warning "Error Mounting HKEY_Local_Machine"}
+if (!(Get-PSDrive -Name HKLM -PSProvider Registry)) {
+    Try {
+        New-PSDrive -Name HKLM -PSProvider Registry -Root HKEY_LOCAL_MACHINE
+    } Catch {
+        Write-Warning "Error Mounting HKEY_Local_Machine"
+    }
 }
 $bv = ("bam", "bam\State")
-Try{$Users = foreach($ii in $bv){Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Services\$($ii)\UserSettings\" | Select-Object -ExpandProperty PSChildName}}
-Catch{
+Try {
+    $Users = foreach ($ii in $bv) { Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Services\$($ii)\UserSettings\" | Select-Object -ExpandProperty PSChildName } }
+Catch {
     Write-Warning "Error Parsing BAM Key. Likely unsupported Windows Version"
     Exit
 }
-$rpath = @("HKLM:\SYSTEM\CurrentControlSet\Services\bam\","HKLM:\SYSTEM\CurrentControlSet\Services\bam\state\")
+$rpath = @("HKLM:\SYSTEM\CurrentControlSet\Services\bam\", "HKLM:\SYSTEM\CurrentControlSet\Services\bam\state\")
 
 $UserTime = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation").TimeZoneKeyName
 $UserBias = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation").ActiveTimeBias
 $UserDay = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation").DaylightBias
 
-Write-Host -ForegroundColor Cyan "Loading BAM History, please wait. Will take a bit."
-$Bam = Foreach ($Sid in $Users){$u++
-        foreach($rp in $rpath){
-           $BamItems = Get-Item -Path "$($rp)UserSettings\$Sid" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Property
+Write-Progress -Activity "Extracting BAM keys" -Status "Please wait..." -PercentComplete 0
 
-            Try{
+$progressTotal = $Users.Count * $rpath.Count
+$progressCounter = 0
+
+$Bam = foreach ($Sid in $Users) {
+    foreach ($rp in $rpath) {
+        $BamItems = Get-Item -Path "$($rp)UserSettings\$Sid" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Property
+
+        Write-Progress -Activity "Extracting BAM keys" -Status "Please wait..." -PercentComplete (($progressCounter++ / $progressTotal) * 100)
+
+        try {
             $objSID = New-Object System.Security.Principal.SecurityIdentifier($Sid)
-            $User = $objSID.Translate( [System.Security.Principal.NTAccount]) 
+            $User = $objSID.Translate([System.Security.Principal.NTAccount])
             $User = $User.Value
-            }
-            Catch{$User=""}
-            $i=0
-            ForEach ($Item in $BamItems){$i++
-		    $Key = Get-ItemProperty -Path "$($rp)UserSettings\$Sid" -ErrorAction SilentlyContinue| Select-Object -ExpandProperty $Item
-	
-            If($key.length -eq 24){
-                $Hex=[System.BitConverter]::ToString($key[7..0]) -replace "-",""
-                $TimeLocal = Get-Date ([DateTime]::FromFileTime([Convert]::ToInt64($Hex, 16))) -Format "yyyy-MM-dd HH:mm:ss"
-			    $TimeUTC = Get-Date ([DateTime]::FromFileTimeUtc([Convert]::ToInt64($Hex, 16))) -Format "yyyy-MM-dd HH:mm:ss"
-			    $Bias = -([convert]::ToInt32([Convert]::ToString($UserBias,2),2))
-			    $Day = -([convert]::ToInt32([Convert]::ToString($UserDay,2),2)) 
-			    $Biasd = $Bias/60
-			    $Dayd = $Day/60
-			    $TImeUser = (Get-Date ([DateTime]::FromFileTimeUtc([Convert]::ToInt64($Hex, 16))).addminutes($Bias) -Format "yyyy-MM-dd HH:mm:ss") 
-			    $d = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}')
-			    {((split-path -path $item).Remove(23)).trimstart("\Device\HarddiskVolume")} else {$d = ""}
-			    $f = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}')
-			    {Split-path -leaf ($item).TrimStart()} else {$item}	
-			    $cp = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}')
-			    {($item).Remove(1,23)} else {$cp = ""}
-			    $path = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}')
-			    {Join-Path -Path "C:" -ChildPath $cp} else {$path = ""}			
-			    $sig = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}')
-			    {Get-Signature -FilePath $path} else {$sig = ""}				
-                [PSCustomObject]@{
-                            'Examiner Time' = $TimeLocal
-						    'Last Execution Time (UTC)'= $TimeUTC
-						    'Last Execution User Time' = $TimeUser
-						     Application = 	$f
-						     Path =  		$path
-                             Signature =          $Sig
-						     User =         $User
-						     SID =          $Sid
-                             Regpath =        $rp
-                             }}}}}
+        } catch {
+            $User = ""
+        }
 
-$Bam | Out-GridView -PassThru -Title "BAM key entries $($Bam.count)  - User TimeZone: ($UserTime) -> ActiveBias: ( $Bias) - DayLightTime: ($Day)"
+        foreach ($Item in $BamItems) {
+            $Key = Get-ItemProperty -Path "$($rp)UserSettings\$Sid" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty $Item
+
+            If ($key.length -eq 24) {
+                $Hex = [System.BitConverter]::ToString($key[7..0]) -replace "-", ""
+                $TimeLocal = Get-Date ([DateTime]::FromFileTime([Convert]::ToInt64($Hex, 16))) -Format "yyyy-MM-dd HH:mm:ss"
+                $Bias = -([convert]::ToInt32([Convert]::ToString($UserBias,2),2))
+                $Day = -([convert]::ToInt32([Convert]::ToString($UserDay,2),2))
+                $Biasd = $Bias/60
+                $Dayd = $Day/60
+                $d = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}') {
+                    ((split-path -path $item).Remove(23)).trimstart("\Device\HarddiskVolume")
+                } else {
+                    $d = ""
+                }
+                $f = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}') {
+                    Split-path -leaf ($item).TrimStart()
+                } else {
+                    $item
+                }
+                $cp = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}') {
+                    ($item).Remove(1,23)
+                } else {
+                    $cp = ""
+                }
+                $path = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}') {
+                    Join-Path -Path "C:" -ChildPath $cp
+                } else {
+                    $path = ""
+                }
+                $sig = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}') {
+                    Get-Signature -FilePath $path
+                } else {
+                    $sig = ""
+                }
+                [PSCustomObject]@{
+                    'Examiner Time' = $TimeLocal
+                    'Last Execution User Time' = $TimeUser
+                    Application = $f
+                    Path = $path
+                    Signature = $Sig
+                    User = $User
+                }
+            }
+        }
+    }
+}
+
+$Bam | Out-GridView -PassThru -Title "BAM key entries $($Bam.count) - some cool bam script lol"
 
 $sw.stop()
 $t = [math]::Round($sw.Elapsed.TotalMinutes, 2)
 
 Clear-Host
 Write-Host "Elapsed Time $t Minutes" -ForegroundColor DarkRed
-
